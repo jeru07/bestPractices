b3df48750ceb03b217c9df1d5d2c2dd9f8785135
jeru07 <jerushaciana719@gmail.com>
2022-06-07 10:31:25 +0530

    add log manager

--- src/com/chainsys/bestPractices/entry/Main.java
+++ src/com/chainsys/bestPractices/entry/Main.java
@@ -2,7 +2,8 @@ package com.chainsys.bestPractices.entry;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		System.out.println("welcome back");
 
